ceb081fac3a71279f868a8654f13c69db5420e4d
Patricia Garcia <martinez171214@gmail.com>
2022-12-12 08:51:29 +0100

    frontend add

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+import path from 'path'
 import express from 'express';
 import 'dotenv/config';
 import openaiRoutes from './routes/openaiRoutes.js'
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))
 app.use('/openai', openaiRoutes);
 
 app.listen(PORT, () => console.log(`🚀 Server is running on port ${PORT}`));
